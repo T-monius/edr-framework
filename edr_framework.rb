7137620f48f5f03c6a8efb6b36a8da2be491df61
Torrel Moseley <trrl.mo@hotmail.com>
2024-06-08 13:41:59 -0500

    create file

--- edr_framework.rb
+++ edr_framework.rb
@@ -6,6 +6,9 @@ class EDRFramework
       path = args[args.index('-p') + 1]
       pid = fork do
         puts "Process started for #{path}"
+        f = File.open(path, 'a')
+        puts "#{path} created" if File.file?(path)
+        f.close
       end
 
       Process.detach(pid)
